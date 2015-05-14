7a2b44ef44245f735911d6fe97cc4c0bcc51ac06
zizuoliu <liuzizuo@gmail.com>
2015-05-13 18:42:53 -0700

    initial commit

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('08MeanApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
